42f0d4e6621b7b9600f2a8531252382c9c11255e
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-05-14 10:28:48 -0600

    3535

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
+	title: `Sapiens IA Laboratories | Building the next generaiton of workers`,
+	description: `A simple, clean, and modern playground for show-casing our AI models' Basilisk AI.`,
 	url: `https://odyssey-theme-eight.vercel.app`, // No trailing slash!
 	name: `SapiensIA`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
