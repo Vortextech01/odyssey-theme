0d3d4e5596f3b960cf95e396de471bb43e02c4db
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-05-14 10:38:01 -0600

    fd

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -1,44 +1,22 @@
 export const footerSocials = [
   {
     name: 'Twitter',
-    url: 'https://twitter.com/littlesticksdev',
+    url: 'https://sapiens-ia.com/chess',
     icon: "mdi:twitter",
   },
   {
     name: 'Youtube',
-    url: 'https://youtube.com/c/LittleSticks',
+    url: 'https://sapiens-ia.com/enterprise',
     icon: "mdi:youtube",
   },
   {
-    name: 'Github',
-    url: 'https://github.com/littlesticks',
-    icon: "mdi:github",
-  },
-  {
     name: 'Discord',
-    url: 'https://littlesticks.dev/discord',
+    url: 'https://sapiens-ia.com/sapiens-journey',
     icon: "ic:baseline-discord",
   },
 ]
 
 export const footerLists = [
-	{
-		title: 'Landing Pages',
-		items: [
-			{
-				title: 'Landing Page 1',
-				slug: '/landing-pages/landing-1',
-			},
-			{
-				title: 'Landing Page 2',
-				slug: '/landing-pages/landing-2',
-			},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		],
-	},
 	{
 		title: 'Company',
 		items: [
@@ -57,24 +35,12 @@ export const footerLists = [
 		],
 	},
 	{
-		title: 'Theme',
+		title: 'Laboratories',
 		items: [
 			{
 				title: 'Get Started',
 				slug: '/theme/get-started',
 			},
-			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
-			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
 		],
 	},
 ];
