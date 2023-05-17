28fc7061c4a3996a590ba6b2d138ca7dd3b4ff36
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-05-17 01:02:09 -0600

    45

--- theme/src/components/theme-switcher/playgroundswitch.ts
+++ theme/src/components/theme-switcher/playgroundswitch.ts
@@ -2,128 +2,146 @@ import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 import {
-  classicThemeIcon,
-  darkThemeIcon,
-  orangeThemeIcon,
+	classicThemeIcon,
+	darkThemeIcon,
+	earthThemeIcon,
+	blueThemeIcon,
+	orangeThemeIcon,
 } from './icons';
 
 const themes = [
   {
     name: 'default',
     icon: classicThemeIcon,
-    label: 'Starter',
+    label: 'Basic Plan',
   },
   {
     name: 'dark',
     icon: darkThemeIcon,
+    label: 'Starter',
+  },
+  {
+    name: 'earth',
+    icon: earthThemeIcon,
     label: 'Business',
+  },
+  {
+    name: 'ocean',
+    icon: blueThemeIcon,
+    label: 'Enterprise',
   },
   {
     name: 'sand',
     icon: orangeThemeIcon,
-    label: 'Enterprise',
+    label: 'Investors',
   }
-];
+]
 
 @customElement('theme-switcher')
 export class ThemeSwitcher extends LitElement {
-  static styles = [
-    css`
-      :host {
-        display: block;
-      }
-      button {
-        display: inline-flex;
-        outline: none;
-        border: none;
-        background-color: transparent;
-        border: 2px solid transparent;
-        border-radius: 20rem;
-        padding: 1px;
-        cursor: pointer;
-        transition: border var(--theme-transition);
-      }
-      button[active] {
-        border: 2px solid var(--theme-primary);
+	static styles = [
+		css`
+			:host {
+				display: block;
+			}
+			button {
+				display: inline-flex;
+				outline: none;
+				border: none;
+				background-color: transparent;
+				border: 2px solid transparent;
+				border-radius: 20rem;
+				padding: 1px;
+				cursor: pointer;
+				transition: border var(--theme-transition);
+			}
+			button[active] {
+				border: 2px solid var(--theme-primary);
         box-shadow: 0 0 12px 1px var(--theme-primary);
-      }
-      button:hover {
-        border: 2px solid var(--theme-primary);
-      }
-      .theme-switcher__container {
-        margin: 2rem 0;
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-      }
-      .theme-select__container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-      }
-      .theme-select__container p {
-        font-size: var(--font-size-sm);
-      }
-    `,
-  ];
+			}
+			button:hover {
+				border: 2px solid var(--theme-primary);
+			}
+			.theme-switcher__container {
+				margin: 2rem 0;
+				display: grid;
+				grid-template-columns: repeat(5, 1fr);
+			}
+			.theme-select__container {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				justify-content: center;
+			}
+			.theme-select__container p {
+				font-size: var(--font-size-sm);
+			}
+		`,
+	];
 
-  // set the _doc element
-  private _doc = document.firstElementChild;
+	// set the _doc element
+	private _doc = document.firstElementChild;
 
-  @property({ type: String })
-  theme: string | null = null;
+	@property({ type: String })
+	theme: string | null = null;
 
-  private _getCurrentTheme() {
-    // check for a local storage theme first
-    const localStorageTheme = localStorage.getItem('theme');
-    if (localStorageTheme !== null) {
-      this._setTheme(localStorageTheme);
-    } else {
+	private _getCurrentTheme() {
+		// check for a local storage theme first
+		const localStorageTheme = localStorage.getItem('theme');
+		if (localStorageTheme !== null) {
+			this._setTheme(localStorageTheme);
+		} else {
       this._setTheme('default');
     }
-  }
+	}
 
   firstUpdated() {
     this._getCurrentTheme();
   }
 
-  private _setTheme(theme) {
-    this._doc.setAttribute('data-theme', theme);
+	private _setTheme(theme) {
+		this._doc.setAttribute('data-theme', theme);
 
     const _heroImage = document.querySelector('#home-hero-image') as HTMLImageElement;
-    if (theme === 'default') {
-      _heroImage.src = '/assets/images/home/square.gif';
-    }
-    if (theme === 'dark') {
-      _heroImage.src = '/assets/images/home/spin.gif';
-    }
-    if (theme === 'sand') {
-      _heroImage.src = '/assets/images/home/orange.gif';
-    }
-    localStorage.setItem('theme', theme);
-    this.theme = theme;
-  }
+		if (theme === 'default') {
+			_heroImage.src = '/assets/images/home/square.gif';
+		}
+		if (theme === 'dark') {
+			_heroImage.src = '/assets/images/home/black.gif';
+		}
+		if (theme === 'earth') {
+			_heroImage.src = '/assets/images/home/orange.gif';
+		}
+		if (theme === 'ocean') {
+			_heroImage.src = '/assets/images/home/undertale.gif';
+		}
+		if (theme === 'sand') {
+			_heroImage.src = '/assets/images/home/Porfolio Profile.gif';
+		}
+		localStorage.setItem('theme', theme);
+		this.theme = theme;
+	}
 
-  render() {
+	render() {
     const themeButtons = html`${themes.map((theme) => {
       return html`
-        <div class="theme-select__container">
-          <button
-            @click=${() => this._setTheme(theme.name)}
-            ?active=${this.theme === theme.name}
-            title=${`Enable ${theme.label} Theme`}
-          >
-            ${theme.icon}
-          </button>
-          <p>${theme.label}</p>
+      <div class="theme-select__container">
+        <button
+          @click=${() => this._setTheme(theme.name)}
+          ?active=${this.theme === theme.name}
+          title=${`Enable ${theme.label} Theme`}
+        >
+          ${theme.icon}
+        </button>
+        <p>${theme.label}</p>
         </div>
-      `;
-    })}`;
+      `
+    })}`
 
-    return html`
-      <div class="theme-switcher__container">
-        ${themeButtons}
-      </div>
-    `;
-  }
+		return html`
+			<div class="theme-switcher__container">
+				${themeButtons}
+			</div>
+		`;
+	}
 }
